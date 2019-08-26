b176c6e8d2f43c3dcbe3eb554f67730cf3671751
Pop Adriana <anni.ania19@yahoo.com>
2019-08-26 21:33:18 +0300

    studied, classes, variables, primitive data types and aggregation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,10 +4,52 @@ package org.fasttrackit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+
+        Car car = new Car();
+        car.name = "Dacia";
+        car.fuelLevel = 60;
+        car.milage = 8.5;
+        car.doorCount = 5;
+        car.maxSpeed = 180;
+        car.fuelType = "Gasoline";
+
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1600;
+
+        car.engine = engine;
+
+        System.out.println(car.name);
+        System.out.println(car.traveledDistance);
+        System.out.println(car.running);
+
+        Car car2 = new Car();
+        car2.name = "Golf";
+        car.fuelLevel = 75;
+        car.milage = 9.7;
+        car.doorCount = 8;
+        car.maxSpeed = 230;
+        car.fuelType = "High";
+
+        car.name = "Skoda";
+        car.name = null;
+
+        //concatenantion
+
+
+        System.out.println("Car2 name" + car2.name);
+        System.out.println("Car2 name" + car.name);
+
+        car = car2;
+
+
+
     }
-}
+        // this is a one-line comment
+    }
+
+
+
--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -1,4 +1,15 @@
 package org.fasttrackit;
 
 public class Car {
+
+    String name;
+    double milage;
+    double maxSpeed;
+    String fuelType;
+    double fuelLevel;
+    double traveledDistance;
+    int doorCount;
+    boolean running;
+    // has-a realtionship
+    Engine engine;
 }
