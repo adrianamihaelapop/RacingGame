edcfd30b5030119f73ecb26e35135167d698d32f
Pop Adriana <anni.ania19@yahoo.com>
2019-09-30 19:09:26 +0300

    added sout

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -62,9 +62,11 @@ public class Game {
         boolean winnerNotKnown = true;
         int competitorsWithoutFuel = 0;
         while (winnerNotKnown && competitorsWithoutFuel < competitors.size()) {
+            System.out.println("New round...\n");
 
             // for-each or enhanced for loop
             for (Vehicle vehicle : competitors) {
+                System.out.println("It's  " + vehicle.getName() + " 's turn");
                 double speed = getAccelerationSpeedFromUser();
                 vehicle.accelerate(60, 1);
                 if (selectedTrack.getLength() < vehicle.getTraveledDistance()) {
